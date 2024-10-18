1993ad7eabe13418a9f6c6defb6b678916e459df
Giacomo Rossettini <rossegiac@gmail.com>
2024-10-18 10:08:03 +0200

    routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
+import { CompletedComponent } from './pages/completed/completed.component';
+import { UsersComponent } from './pages/users/users.component';
+import { Route } from '@angular/router';
+
+const routes: Route[] = [
+  {
+    path: "",
+    component: MainComponent
+  },
+  {
+    path: "completed",
+    component: CompletedComponent
+  },
+  {
+    path: "users",
+    component: UsersComponent
+  }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    CompletedComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/models/user.ts
@@ -0,0 +1,8 @@
+export interface User {
+  id:1,
+  firstName: string,
+  lastName: string,
+  email: string,
+  image: string,
+  title: string
+}
