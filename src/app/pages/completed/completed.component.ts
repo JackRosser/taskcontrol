ee5415eed57102e2bcd6d5d41b86b832ca46517d
Giacomo Rossettini <rossegiac@gmail.com>
2024-10-18 15:53:28 +0200

    task completi

--- src/app/pages/completed/completed.component.ts
+++ src/app/pages/completed/completed.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { iUnited } from '../../models/united';
+import { iTask } from '../../models/task';
+import { UserservicesService } from '../../services/userservices.service';
+import { TaskservicesService } from '../../services/taskservices.service';
 
 @Component({
   selector: 'app-completed',
@@ -6,5 +10,35 @@ import { Component } from '@angular/core';
   styleUrl: './completed.component.scss'
 })
 export class CompletedComponent {
+
+constructor(private globalUsers:UserservicesService, private globalTask:TaskservicesService) {}
+
+
+  allUsersAndTasks: iUnited[] = [];
+  taskEseguito: boolean = false;
+
+
+
+  ngOnInit() {
+    this.globalUsers.allUsers$.subscribe(allGlobalUsers => {
+      this.globalTask.allTasks$.subscribe(allGlobalTasks => {
+        this.allUsersAndTasks = allGlobalUsers.map(user => ({
+          ...user,
+          tasks: []
+        }));
+
+        this.allUsersAndTasks.forEach((user: iUnited) => {
+          allGlobalTasks.forEach((task: iTask) => {
+            if (Number(user.id) === Number(task.userId)) {
+              user.tasks?.push(task);
+            }
+          });
+        });
+
+        this.allUsersAndTasks.sort((a, b) => a.firstName.localeCompare(b.firstName));
+      });
+    });
+  }
+
 
 }
